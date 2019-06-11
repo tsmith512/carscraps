0d127118de46f7152eb38e03365aa828196adf8c
Taylor Smith <tsmith@tsmithcreative.com>
2019-06-11 21:16:06 +0000

    Scrap the hello world and add an API endpoint to receive POSTS from Slack

--- src/Controller/CarScrapsReceiver.php
+++ src/Controller/CarScrapsReceiver.php
@@ -6,12 +6,28 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
-
+/**
+ * Class offers API endpoints related to receiving requests and scraping
+ * requested URLs. Currently only supports a handful of Slack App events.
+ */
 class CarScrapsReceiver {
   /**
-   * @Route("/scrape")
+   * @Route("/api/v1/slack", methods={"POST"})
    */
-  public function scrape() {
-    return new Response("This is a test");
+  public function slack() {
+    $request = Request::createFromGlobals();
+    $input = json_decode($request->getContent(), true);
+
+    // Bail if there was no request object from Slack.
+    if (empty($input)) {
+      return new Response('Bad request: request payload was empty', 400);
+    }
+
+    switch ($input['type']) {
+      // Slack Apps are subject to URL verification. For this test, just return the challenge string.
+      case "url_verification":
+        return new Response($input['challenge']);
+        break;
+    }
   }
 }
