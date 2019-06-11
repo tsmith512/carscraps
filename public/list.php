f22ebad7c3a3b6a5227cd2bc79d1cbdfb0aacd4d
Taylor Smith <tsmith@tsmithcreative.com>
2019-06-11 23:21:46 +0000

    Update paths in the legacy cron script to work with the new structure

--- public/list.php
+++ public/list.php
@@ -8,12 +8,12 @@
 
 <?php
 
-$handle = fopen("../index.txt", "r");
+$handle = fopen("index.txt", "r");
 if ($handle) {
   echo "<ul>";
   while (($line = fgets($handle)) !== false) {
     $parts = explode(":", $line);
-    $file = str_replace("htdocs/mirror", "", $parts[0]);
+    $file = str_replace("public/mirror", "", $parts[0]);
     $title = preg_replace("/<\/?title>/", "", trim($parts[1]));
     echo "<li><a href=\"/mirror/{$file}\">{$title}</a></li>";
   }
