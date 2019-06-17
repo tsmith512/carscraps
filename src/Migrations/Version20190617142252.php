fe5081a73982a06b1c8d70f9c1140a6388c1bfb1
Taylor Smith <tsmith@tsmithcreative.com>
2019-06-17 16:51:35 +0000

    Now migrate over the slack timestamp values and clear out timestamp column

--- src/Migrations/Version20190617142252.php
+++ src/Migrations/Version20190617142252.php
@@ -23,6 +23,12 @@ final class Version20190617142252 extends AbstractMigration
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
         $this->addSql('ALTER TABLE car ADD slackts VARCHAR(255) DEFAULT NULL');
+
+        // Slack wants its timestamp stored as a string not a float (see their
+        // API documentation for more info). Copy it to the new column, then
+        // unset timestamp.
+        $this->addSql('UPDATE car SET slackts = timestamp');
+        $this->addSql('UPDATE car SET timestamp = NULL');
     }
 
     public function down(Schema $schema) : void
